e12e8af359baa00aafe17afe7a117dcb8703a177
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-08 17:02:00 +0530

    ADDED: Program for Password Validation Rule2

--- src/main/java/com/userresistration/UserDetails.java
+++ src/main/java/com/userresistration/UserDetails.java
@@ -8,7 +8,7 @@ public class UserDetails {
 	private static final String lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*[@]{1}[a-z]+[.]{1}[a-z]{2,4}[.]*[a-z]*{2}$";
 	private static final String MOBILE_NUMBER = "^[9]{1}[0-9]{1}[ ][789]{1}[0-9]{9}$";
-	private static final String PASSWORD = "^([A-Za-z0-9!@#$%^&*.]{8,})$";
+	private static final String PASSWORD = "^([A-Z]{1,}[A-Za-z0-9!@#$%^&*.]{7,})$";
 
 	public boolean validateFirstName(String fName) {
 		boolean result = Pattern.matches(firstName, fName);
