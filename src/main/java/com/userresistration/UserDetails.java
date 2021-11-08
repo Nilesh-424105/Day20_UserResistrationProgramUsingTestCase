b6e9609a2420746410ba0263637a40118aebba61
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-08 15:19:17 +0530

    ADDED: Welcome to User Resistration Program

---
+++ src/main/java/com/userresistration/UserDetails.java
@@ -0,0 +1,10 @@
+package com.userresistration;
+
+public class UserDetails {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to User Resistration Junit Program");
+	}
+
+}
